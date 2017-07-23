a9cc42ab9b0acc4ee50917f6e74e45282709f2a5
Nikolas Moyseos <nikolasmoyseos@gmail.com>
2017-07-23 12:08:58 -0400

    add comp

--- js/App.js
+++ js/App.js
@@ -1,13 +1,15 @@
 const React = require("react");
 const Title = require("./components/Title");
 const Description = require("./components/Description");
+const Color = require("./components/Color");
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="app">
 				<Title>Test Title</Title>
-				<Description library="will.js"/>
+				<Description library="Nick.js"/>
+				<Color/>
 			</div>
 		);
 	}
---
+++ js/components/Color.js
@@ -0,0 +1,14 @@
+const React = require("react");
+
+class Color extends React.Component {
+	render() {
+		const libName = "React";
+
+		return (
+			<font className="app-font" size="5"  color="yellow">My Cool Font Thing</font>
+		);
+	}
+}
+
+
+module.exports = Color;
