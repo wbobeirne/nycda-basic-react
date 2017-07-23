39b9dd6e250d02a544684bbf60d219051ddcb953
Will O'Beirne <wbobeirne@gmail.com>
2017-07-23 11:55:38 -0400

    Componentize

--- js/App.js
+++ js/App.js
@@ -1,17 +1,13 @@
 const React = require("react");
+const Title = require("./components/Title");
+const Description = require("./components/Description");
 
 class App extends React.Component {
 	render() {
-		const libName = "React";
-
 		return (
 			<div className="app">
-				<h1 className="app-title">Hello!</h1>
-				<p className="app-description">
-					This is {libName}. Even though we don’t have any elements
-					on the page to start, {libName} quickly fills in the
-					javascript content.
-				</p>
+				<Title>Test Title</Title>
+				<Description library="will.js"/>
 			</div>
 		);
 	}
