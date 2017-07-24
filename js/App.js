1d20ec6563b614b301238267fd7049b5913616d7
Alex Polotsky <polonyc77@gmail.com>
2017-07-24 10:20:15 -0400

    no changes

--- js/App.js
+++ js/App.js
@@ -2,6 +2,7 @@ const React = require("react");
 const Title = require("./components/Title");
 const Description = require("./components/Description");
 const Font = require("./components/Font");
+const Counter = require("./components/Counter");
 
 class App extends React.Component {
 	render() {
@@ -10,6 +11,14 @@ class App extends React.Component {
 				<Title>Test Title</Title>
 				<Description library="will.js"/>
 				<Font color="blue" size="8">Here I am</Font>
+
+
+				<p>
+					You have been on my site for <Counter start={0}/> seconds
+				</p>
+
+				<Font color="blue" size="8">Here I am</Font>
+
 			</div>
 		);
 	}
